23cc4e3d2f6311e034c8739d4a9f94b892235c46
mcdanije22 <35205212+mcdanije22@users.noreply.github.com>
2018-07-04 20:36:53 -0400

    Add files via upload
    delete button deletes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var check = document.getElementById("check");
 var input = document.getElementById("userInput");
-var ul = document.getElementById("list");
+var itemList = document.getElementById("list");
 var enter = document.getElementById("enter");
 var add = document.getElementsByClassName("add");
 
@@ -10,7 +10,7 @@ function inputLength () {
 function addList() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
+	itemList.appendChild(li);
 	var button = document.createElement("button");
 	button.appendChild(document.createTextNode(" "));
 	li.appendChild(button);
@@ -20,8 +20,6 @@ function addList() {
 	li.appendChild(buttonTwo);
 	buttonTwo.setAttribute("class","del far fa-times-circle");
 	input.value = "";
-
-	var deleteBtn = document.createElement("del");
 }
 function afterClick (){
 	if (inputLength() > 0) {
@@ -39,8 +37,23 @@ function keyPressadd(event) {
 }
 enter.addEventListener("click",afterClick);
 input.addEventListener("keypress",keyPressadd);
+itemList.addEventListener("click",removeItem);
 
 function done (){
 	document.getElementByClassNames("btn").style.backgroundColor = "#4CAF50"
 }
-//btn.addEventListener("click",done);
+	/*function removeItem (e) {
+		var li = e.target.parentElement;
+		itemList.removeChild(li);
+	}*/
+
+	function removeItem(e){
+		if(e.target.classList.contains("del")){
+			var li = e.target.parentElement;
+			itemList.removeChild(li);
+		}else if (e.target.classList.contains("add")){			
+	itemList.style.backgroundColor = "#4CAF50"
+
+		}
+	}
+	
