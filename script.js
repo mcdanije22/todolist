f00bbc1917cd269cb6b2a17dfce3fdeae7a12f3d
mcdanije22 <35205212+mcdanije22@users.noreply.github.com>
2018-07-06 22:59:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-var check = document.getElementById("check");
-var input = document.getElementById("userInput");
-var itemList = document.getElementById("list");
-var enter = document.getElementById("enter");
-var add = document.getElementsByClassName("add");
+let check = document.getElementById("check");
+let input = document.getElementById("userInput");
+let itemList = document.getElementById("list");
+let enter = document.getElementById("enter");
+let add = document.getElementsByClassName("add");
 
 function inputLength () {
 	return input.value.length;
 }
 function addList() {
-	var li = document.createElement("li");
+	let li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	itemList.appendChild(li);
-	var button = document.createElement("button");
-	button.appendChild(document.createTextNode(" "));
-	li.appendChild(button);
-	button.setAttribute("class","add");
-	var buttonTwo = document.createElement("button");
+	let buttonTwo = document.createElement("button");
 	buttonTwo.appendChild(document.createTextNode(""));
 	li.appendChild(buttonTwo);
 	buttonTwo.setAttribute("class","del far fa-times-circle");
@@ -25,35 +21,25 @@ function afterClick (){
 	if (inputLength() > 0) {
 		addList();
 	} else {
-		alert("Please enter item first");
+		alert("Please enter an item first");
 	}
 }
 function keyPressadd(event) {
 	if(inputLength() > 0 && event.keyCode === 13){
 		addList();
 	} else if (inputLength() <= 0 && event.keyCode === 13){
-		alert("Please enter item first");
+		alert("Please enter an item first");
 	}
 }
 enter.addEventListener("click",afterClick);
 input.addEventListener("keypress",keyPressadd);
 itemList.addEventListener("click",removeItem);
 
-function done (){
-	document.getElementByClassNames("btn").style.backgroundColor = "#4CAF50"
-}
-	/*function removeItem (e) {
-		var li = e.target.parentElement;
-		itemList.removeChild(li);
-	}*/
 
 	function removeItem(e){
 		if(e.target.classList.contains("del")){
-			var li = e.target.parentElement;
+			let li = e.target.parentElement;
 			itemList.removeChild(li);
-		}else if (e.target.classList.contains("add")){			
-	itemList.style.backgroundColor = "#4CAF50"
-
 		}
 	}
 	
